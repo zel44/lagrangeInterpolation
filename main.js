5b81e8b9cdb535c06f18c0599c3fb807c2941901
Zel Zel <Zel Zel>
2017-05-26 15:12:05 +0300

    third commit

--- main.js
+++ main.js
@@ -1,39 +1,48 @@
 var step = 0.01, values ={};
 
-	var lanPol = function(x, x_values, y_values, size)
-	{
-		var lagrange_pol = 0;
-		var basics_pol;
+var lanPol = function(x, x_values, y_values, size)
+{
+	var lagrange_pol = 0;
+	var basics_pol;
 
-		for (var i = 0; i < size; i++)
+	for (var i = 0; i < size; i++)
+	{
+		basics_pol = 1;
+		for (var j = 0; j < size; j++)
 		{
-			basics_pol = 1;
-			for (var j = 0; j < size; j++)
-			{
-				if (j == i) continue;
-				basics_pol *= (x - x_values[j])/(x_values[i] - x_values[j]);
-			}
-			lagrange_pol += basics_pol*y_values[i];
+			if (j == i) continue;
+			basics_pol *= (x - x_values[j])/(x_values[i] - x_values[j]);
 		}
-		return lagrange_pol;
-	};
+		lagrange_pol += basics_pol*y_values[i];
+	}
+	return lagrange_pol;
+};
 
 var getValues = function() {
-		values = {}
-		initValues = function(coordName, defaultValues) {
-			values[coordName] = []
-			document.getElementById(coordName).value.split(' ').forEach(function(value){
-				values[coordName].push(parseFloat(value));
-			});
-			if (values[coordName].length === 0 || isNaN(values[coordName][0])) {
-				values[coordName] = defaultValues
-			}
-			values[coordName] = values[coordName].sort();
-		};
-		initValues('x',[1, 3, 4]);
-		initValues('y',[12, 4, 6]);
-		return values;
+	values = {}
+	initValues = function(coordName, defaultValues) {
+		values[coordName] = []
+		document.getElementById(coordName).value.split(' ').forEach(function(value){
+			values[coordName].push(parseFloat(value));
+		});
+		if (values[coordName].length === 0 || isNaN(values[coordName][0])) {
+			values[coordName] = defaultValues
+		}
 	};
+	initValues('x',[1, 3, 4]);
+	initValues('y',[12, 4, 6]);
+	var map = values.x.map(function(e, i) {
+		return { index: i, value: e };
+	});
+	map.sort(function(a, b) {
+		return a.value - b.value;
+	});
+	values.y = map.map(function(e, i) {
+		values.x[i] = e.value;
+		return values.y[e.index];
+	});
+	return values;
+};
 
 (document.getElementById('start')).addEventListener('click', function(){
 
